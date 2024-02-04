d23ac33315bcd2dbdd8447df841b81b90fdcf172
Koujirou513 <kojiro323a@gmail.com>
2024-02-04 11:58:20 +0000

    空のモーダルの表示

--- front/React/src/pages/RecipeList/RecipeList.jsx
+++ front/React/src/pages/RecipeList/RecipeList.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
 import styles from './RecipeList.module.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { SearchBar } from './SearchBar';
 import { TagSelect } from './TagSelect/TagSelect';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { RecipeRegistModal } from './RecipeRegistModal/RecipeRegistModal';
+
 export const RecipeList = () => {
+    const [modalOpen, setModalOpen] = React.useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    }
+
     return (
         <>
             <div className={styles.header}>
@@ -18,7 +27,12 @@ export const RecipeList = () => {
                 <TagSelect />
             </div>
             <div>
-                <AddCircleIcon className={styles.newRecipeIcon} sx={{ fontSize: 60}} color='primary'/>
+                <AddCircleIcon 
+                className={styles.newRecipeIcon} 
+                sx={{ fontSize: 60}} 
+                color='primary'
+                onClick={handleOpenModal} />
+                {modalOpen && <RecipeRegistModal open={modalOpen} setOpen={setModalOpen} />}
             </div>
         </>
     )
