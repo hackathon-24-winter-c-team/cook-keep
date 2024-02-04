9caeca5a9842b8675ac300259305c84a7f006729
Koujirou513 <kojiro323a@gmail.com>
2024-02-04 06:44:02 +0000

    レシピ一覧のヘッダー作成

--- front/React/src/pages/RecipeList/RecipeList.jsx
+++ front/React/src/pages/RecipeList/RecipeList.jsx
@@ -1,7 +1,19 @@
+import styles from './RecipeList.module.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { SearchBar } from './SearchBar';
+
 export const RecipeList = () => {
     return (
         <>
-            <h1>レシピリストページ</h1>
+            <div className={styles.header}>
+                <ul>
+                    <AccountCircleIcon className={styles.userIcon}/>
+                    <SearchBar />
+                    <LogoutIcon className={styles.logoutIcon}/>
+                </ul>
+            </div>
+
         </>
     )
 }
