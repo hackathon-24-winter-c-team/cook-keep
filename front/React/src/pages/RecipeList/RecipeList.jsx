b411c55306dac548854b9c5a2a0265c271593c27
Koujirou513 <kojiro323a@gmail.com>
2024-02-04 09:23:18 +0000

    レシピページのタグ、アイコン表示

--- front/React/src/pages/RecipeList/RecipeList.jsx
+++ front/React/src/pages/RecipeList/RecipeList.jsx
@@ -2,18 +2,24 @@ import styles from './RecipeList.module.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { SearchBar } from './SearchBar';
-
+import { TagSelect } from './TagSelect/TagSelect';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 export const RecipeList = () => {
     return (
         <>
             <div className={styles.header}>
                 <ul>
-                    <AccountCircleIcon className={styles.userIcon}/>
+                    <AccountCircleIcon className={styles.userIcon} fontSize='large'/>
                     <SearchBar />
-                    <LogoutIcon className={styles.logoutIcon}/>
+                    <LogoutIcon className={styles.logoutIcon} />
                 </ul>
             </div>
-
+            <div>
+                <TagSelect />
+            </div>
+            <div>
+                <AddCircleIcon className={styles.newRecipeIcon} sx={{ fontSize: 60}} color='primary'/>
+            </div>
         </>
     )
 }
