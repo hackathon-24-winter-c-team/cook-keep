02e0ae048c06184287f3056d2ee1ca7a7ea91b28
Taka <takahito.uchino.1218@gmail.com>
2024-02-09 21:41:53 +0900

    Userモデルを管理画面に追加

--- backend/django_app/accounts/admin.py
+++ backend/django_app/accounts/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("username", "email", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+    list_filter = ("is_staff", "is_superuser")
