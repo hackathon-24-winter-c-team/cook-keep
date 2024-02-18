5856f61aa650142827bc7c7a4fbd38a6fa0ef770
門田康宏 <mondenyasuhiro@kadotayasuhiro.local>
2024-02-18 22:47:48 +0900

    20240218_レシピ追加モーダル修正02

--- front/React/src/pages/RecipeList/RecipeRegistModal/RecipeRegistModal.jsx
+++ front/React/src/pages/RecipeList/RecipeRegistModal/RecipeRegistModal.jsx
@@ -14,7 +14,9 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
+import axios from 'axios';
+/* import { useNavigate } from 'react-router-dom';
+ */
 const style = {
   position: 'absolute',
   top: '50%',
@@ -33,212 +35,231 @@ const style = {
 export const RecipeRegistModal = ({ open, setOpen }) => {
   const handleClose = () => setOpen(false);
 
-  // レシピ名の値を管理するステート
-  const [name, setName] = useState("");
-
-  // レシピ名を更新する関数
-  const inputName = (event) => {
-    setName(event.target.value)
-  };
-
-  // レシピURLを管理するステート
-  const [recipeUrl, setRecipeUrl] = useState("");
-
-  // レシピURLを更新するステート
-  const inputRecipeUrl = (event) => {
-    setRecipeUrl(event.target.value)
+  const recipeInfo = { recipename: "", recipeurl: "", imageurl: "", main: "", genre: "", jitan: "", memo: "" } //レシピ追加モーダルの初期値
+  const [recipeValues, setRecipeValues] = useState(recipeInfo) // レシピ追加モーダルの値の状態
+  const [recipeErrors, setRecipeErrors] = useState({}) // レシピ追加モーダルのエラーの状態
+/*   const navigate = useNavigate() // ナビゲーション関数
+ */
+  // レシピ追加モーダルの入力値が変更された時に呼び出される関数
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRecipeValues({ ...recipeValues, [name]: value })
   }
 
-  // 画像URLを管理するステート
-  const [imageUrl, setImageUrl] = useState("");
+  // 入力値のバリデーションを行い、エラーがあればそれを返す
+  const validate = (values) => {
+    const errors = {};
 
-  // 画像URLを更新するステート
-  const inputImageUrl = (event) => {
-    setImageUrl(event.target.value)
+    if (!values.recipename) {
+      errors.recipename = "レシピ名を入力してください";
+    }
+    if (!values.recipeurl || !values.imageurl) {
+      errors.recipeurl = "レシピURL、もしくは画像URLを入力してください";
+    }
+    if (!values.main) {
+      errors.main = "メインタグを入力してください";
+    }
+    if (!values.genre) {
+      errors.genre = "ジャンルタグを入力してください";
+    }
+    return errors;
   }
 
-  // 各カテゴリの選択された値を管理するステート
-  const [main, setMain] = useState('');
-  const [genre, setGenre] = useState('');
-  const [jitan, setJitan] = useState('');
+  // フォームが送信された時に呼び出される関数
+  const onClickAdd = async () => {
 
+/*     event.preventDefault(); // デフォルトの送信動作を防止
+ */    setRecipeErrors({}); //エラーメッセージーの初期化
+    const errors = validate(recipeValues); // 入力値のバリデーション
+    setRecipeErrors(errors);  //エラー状態の更新
 
-  // 'メイン' カテゴリの選択値を更新する関数
-  const handleMainChange = (event) => {
-    setMain(event.target.value);
-  };
+    // エラーがない場合、登録成功のアラートを表示し、リダイレクトを実行
+    /*     if (Object.keys(errors).length === 0) {
+          try {
+     */        // json-serverにPOSTリクエストを送信
+    const response = await axios.post('http://localhost:3001/recipes', recipeValues
+);
 
-  // 'ジャンル' カテゴリの選択値を更新する関数
-  const handleGenreChange = (event) => {
-    setGenre(event.target.value)
-  }
+    console.log(response.data)
 
-  // '時短' カテゴリの選択値を更新する関数
-  const handleJitanChange = (event) => {
-    setJitan(event.target.value)
-  }
+    /*         // POSTリクエストが成功した場合の処理
+            console.log('Add Success:', response.data);
+            navigate('/recipes');  // レシピ一覧ページへのリダイレクト         
+          } catch (error) {
+            // エラー発生時の処理
+            console.error('Add Error:', error);
+            alert("登録に失敗しました")
+          }
+        }
+      };
+     */
 
-  // メモを管理するステート
-  const [memo, setMemo] = useState("");
+    /*   レシピ追加時の処理
+      const onClickAdd = () => {
+        if (recipeInfo.name === "") {
+          alert("入力値に不足箇所があります。")
+          handleClose();
+          return;
+        }
+        if (recipeInfo.main === "") {
+          alert("入力値に不足箇所があります。")
+          handleClose();
+          return;
+        }
+        if (recipeInfo.genre === "") {
+          alert("入力値に不足箇所があります。")
+          handleClose();
+          return;
+        }
+        if (recipeInfo.jitan === "") {
+          alert("入力値に不足箇所があります。")
+          handleClose();
+          return;
+        }
+    
+        if (recipeInfo.recipeurl === "" || recipeInfo.imageurl === "") {
+          alert("レシピURL、または画像URLを入力して下さい");
+          handleClose();
+          return;
+        }
+    
+        handleSubmit();
+      };
+    */
+      };
+    return (
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h3" component="h2" sx={{ color: 'red', fontWeight: 'bold', textAlign: 'center', mt: 3 }}>
+              新しいレシピ
+            </Typography>
 
-  // メモを更新するステート
-  const inputMemo = (event) => {
-    setMemo(event.target.value)
-  }
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1, width: '25ch', mr: 3 }, textAlign: 'center'
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField id="standard-basic" label="レシピ名 " variant="standard" name="recipename" onChange={(e) => handleChange(e)} ></TextField>
+              <p>{recipeErrors.recipename}</p>
 
+              <TextField id="standard-basic" label="レシピ URL" variant="standard" name="recipeurl" onChange={(e) => handleChange(e)} ></TextField>
+              <p>{recipeErrors.recipeurl}</p>
 
-  /*レシピ追加時の処理*/
-  const onClickAdd = () => {
-    if (name === "") {
-      alert("レシピ名を入力してください")
-      return;
-    }
-    if (main === "") {
-      alert("メインタグを選択して下さい")
-      return;
-    }
-    if (genre === "") {
-      alert("ジャンルタグを選択して下さい")
-      return;
-    }
-    if (jitan === "") {
-      alert("時間タグを選択してください")
-      return;
-    }
+              <TextField id="standard-basic" label="画像 URL" variant="standard" name="imageurl" onChange={(e) => handleChange(e)} ></TextField>
+              <p>{recipeErrors.imageurl}</p>
 
-    if (recipeUrl === "" || imageUrl === "") {
-      alert("レシピURL、または画像URLを入力して下さい");
-      return;
-    }
+            </Box>
 
-    handleClose();
-
-  };
-
-  return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h3" component="h2" sx={{ color: 'red', fontWeight: 'bold', textAlign: 'center', mt: 3 }}>
-            新しいレシピ
-          </Typography>
-
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '25ch', mr: 3 }, textAlign: 'center'
-            }}
-            noValidateÏ
-            autoComplete="off"
-          >
-            <TextField id="standard-basic" label="レシピ名 " variant="standard" value={name} onChange={inputName} ></TextField>
-            <TextField id="standard-basic" label="レシピ URL" variant="standard" value={recipeUrl} onChange={inputRecipeUrl} ></TextField>
-            <TextField id="standard-basic" label="画像 URL" variant="standard" value={imageUrl} onChange={inputImageUrl} ></TextField>
-          </Box>
-
-          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            {/*             <NewTagSelectMain />
+            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+              {/*             <NewTagSelectMain />
             <NewTagSelectGenre />
             <NewTagSelectJitan />
  */}
 
-            <div>
-              {/* 'メイン' カテゴリのセレクトメニュー */}
-              <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
-                <InputLabel id="main-select-label">メイン</InputLabel>
-                <Select
-                  labelId="main-select-label"
-                  id="main-select"
-                  value={main}
-                  label="メイン"
-                  onChange={handleMainChange}
-                >
-                  <MenuItem value="" sx={{ height: 35 }}>
-                    <em></em>
-                  </MenuItem>
-                  <MenuItem value={'rice'}>ご飯</MenuItem>
-                  <MenuItem value={'meat'}>肉</MenuItem>
-                  <MenuItem value={'fish'}>魚</MenuItem>
-                  <MenuItem value={'vegetable'}>野菜</MenuItem>
-                  <MenuItem value={'soup'}>汁物</MenuItem>
-                  <MenuItem value={'dessert'}>デザート</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
+              <div>
+                {/* 'メイン' カテゴリのセレクトメニュー */}
+                <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
+                  <InputLabel id="main-select-label">メイン</InputLabel>
+                  <Select
+                    labelId="main-select-label"
+                    id="main-select"
+                    label="メイン"
+                    value={recipeValues.main}
+                    name="main"
+                    onChange={(e) => handleChange(e)}
+                  >
+                    <MenuItem value="" sx={{ height: 35 }}>
+                      <em></em>
+                    </MenuItem>
+                    <MenuItem value={'rice'}>ご飯</MenuItem>
+                    <MenuItem value={'meat'}>肉</MenuItem>
+                    <MenuItem value={'fish'}>魚</MenuItem>
+                    <MenuItem value={'vegetable'}>野菜</MenuItem>
+                    <MenuItem value={'soup'}>汁物</MenuItem>
+                    <MenuItem value={'dessert'}>デザート</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
 
-            <div>
-              {/* 'ジャンル' カテゴリのセレクトメニュー */}
-              <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
-                <InputLabel id="genre-select-label">ジャンル</InputLabel>
-                <Select
-                  labelId="genre-select-label"
-                  id="genre-select"
-                  value={genre}
-                  label="ジャンル"
-                  onChange={handleGenreChange}
-                >
-                  <MenuItem value="" sx={{ height: 35 }}>
-                    <em></em>
-                  </MenuItem>
-                  <MenuItem value={'japanese'}>和食</MenuItem>
-                  <MenuItem value={'western'}>洋食</MenuItem>
-                  <MenuItem value={'chinese'}>中華</MenuItem>
-                  <MenuItem value={'other'}>その他</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
+              <div>
+                {/* 'ジャンル' カテゴリのセレクトメニュー */}
+                <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
+                  <InputLabel id="genre-select-label">ジャンル</InputLabel>
+                  <Select
+                    labelId="genre-select-label"
+                    id="genre-select"
+                    label="ジャンル"
+                    value={recipeValues.genre}
+                    name="genre"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="" sx={{ height: 35 }}>
+                      <em></em>
+                    </MenuItem>
+                    <MenuItem value={'japanese'}>和食</MenuItem>
+                    <MenuItem value={'western'}>洋食</MenuItem>
+                    <MenuItem value={'chinese'}>中華</MenuItem>
+                    <MenuItem value={'other'}>その他</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
 
-            <div>
-              {/* '時短' カテゴリのセレクトメニュー */}
-              <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
-                <InputLabel id="jitan-select-label">時間</InputLabel>
-                <Select
-                  labelId="jitan-select-label"
-                  id="jitan-select"
-                  value={jitan}
-                  label="時短"
-                  onChange={handleJitanChange}
-                >
-                  <MenuItem value="" sx={{ height: 35 }}>
-                    <em></em>
-                  </MenuItem>
-                  <MenuItem value={'jitan'}>時短</MenuItem>
-                  <MenuItem value={'sonota'}>その他</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
+              <div>
+                {/* '時短' カテゴリのセレクトメニュー */}
+                <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
+                  <InputLabel id="jitan-select-label">時間</InputLabel>
+                  <Select
+                    labelId="jitan-select-label"
+                    id="jitan-select"
+                    label="時短"
+                    value={recipeValues.jitan}
+                    name="jitan"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="" sx={{ height: 35 }}>
+                      <em></em>
+                    </MenuItem>
+                    <MenuItem value={'jitan'}>時短</MenuItem>
+                    <MenuItem value={'sonota'}>その他</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
 
 
+            </Box>
+
+            <Box sx={{ display: 'flex', mb: 2, mt: 2 }}>
+              <TextareaAutosize
+                minRows={10}
+                maxRows={10}
+                aria-label="maximum height"
+                placeholder="memo"
+                style={{ width: "100%", }}
+                name="memo"
+                onChange={handleChange}
+              />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Stack spacing={2} direction="row" sx={{ textAlign: 'center' }}>
+                <Button variant="contained" onClick={onClickAdd}>追加</Button>
+              </Stack>
+            </Box>
           </Box>
+        </Modal >
+      </div>
+    );
+  }
 
-          <Box sx={{ display: 'flex', mb: 2, mt: 2 }}>
-            <TextareaAutosize
-              minRows={10}
-              maxRows={10}
-              aria-label="maximum height"
-              placeholder="memo"
-              style={{ width: "100%", }}
-              value={memo}
-              onChange={inputMemo}
-            />
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Stack spacing={2} direction="row" sx={{ textAlign: 'center' }}>
-              <Button variant="contained" onClick={onClickAdd}>追加</Button>
-            </Stack>
-          </Box>
-        </Box>
-      </Modal >
-    </div>
-  );
-}
-
-RecipeRegistModal.propTypes = {
-  open: PropTypes.bool.isRequired,
-  setOpen: PropTypes.func.isRequired,
-};
+  RecipeRegistModal.propTypes = {
+    open: PropTypes.bool.isRequired,
+    setOpen: PropTypes.func.isRequired,
+  };
