7c0cda3c410f61c9bf0e7efbd6467833d74e655d
門田康宏 <mondenyasuhiro@kadotayasuhiro.local>
2024-02-24 00:47:51 +0900

    20240223_レシピカードURL有効化

--- front/React/src/pages/RecipeCard/RecipeCard.jsx
+++ front/React/src/pages/RecipeCard/RecipeCard.jsx
@@ -7,38 +7,53 @@ import { Box, CardActionArea } from '@mui/material';
 // import { useRecoilValue } from 'recoil';
 // import { recipesState } from '../../state/recipesState';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export const RecipeCard = ({ recipe }) => {
-    // const recipe = useRecoilValue(recipesState);
+export const RecipeCard = ({ recipe, recipeDetail }) => {
+
+    RecipeCard.propTypes = {
+        recipeDetail: PropTypes.func.isRequired, // onClickプロパティの型をfuncとして指定
+        // 他のプロパティも必要に応じて指定
+    };
+
+    const maxLength = 25;
+    let urlLength = [""];
+
+    if (recipe.data_url.length <= maxLength) {
+        urlLength = recipe.data_url
+    } else {
+        urlLength = recipe.data_url.slice(0, maxLength - 3) + "...";
+    }
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-                <Card sx={{ maxWidth: 800, m: '10px' }}>
-                    <CardActionArea>
-                        <CardMedia
-                            component="img"
-                            height="250"
-                            src={recipe.images || "https://picsum.photos/300"}
-                            alt={recipe.recipe_name}
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h6" component="div" sx={{ borderBottom: 1, borderColor: 'grey.500', mb: 2 }}>
-                                {recipe.recipe_name}
-                            </Typography>
-                            <Typography gutterBottom variant="h6" component="div" sx={{ borderBottom: 1, borderColor: 'grey.500' }}>
-                                {recipe.data_url}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
+        <Box sx={{ display: 'inline-flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+            <Card sx={{ maxWidth: 800, m: '10px' }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="250"
+                        src={recipe.images || "https://picsum.photos/300"}
+                        alt={recipe.recipe_name}
+                        onClick={recipeDetail}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h6" component="div" sx={{ borderBottom: 1, borderColor: 'grey.500', mb: 2 }} onClick={recipeDetail}>
+                            {recipe.recipe_name}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div" sx={{ borderBottom: 1, borderColor: 'grey.500', mb: 2 }} >
+                            <Link target="_blank" to={recipe.data_url} sx={{ borderBottom: 1, borderColor: 'grey.500' }}>{urlLength}</Link>
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
         </Box>
     );
 };
 
 RecipeCard.propTypes = {
     recipe: PropTypes.shape({
-      recipe_name: PropTypes.string,
-      data_url: PropTypes.string,
-      images: PropTypes.string, 
+        recipe_name: PropTypes.string,
+        data_url: PropTypes.string,
+        images: PropTypes.string,
     }),
-  };
+};
