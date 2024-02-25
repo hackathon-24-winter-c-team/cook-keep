210999963c1e0e7148add0dbeab0300f4e007053
Taka <takahito.uchino.1218@gmail.com>
2024-02-25 14:29:04 +0900

    APIの設定の一通り

--- backend/django_app/django_app/urls.py
+++ backend/django_app/django_app/urls.py
@@ -7,7 +7,7 @@ from api import urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api-auth/login/recipes/", include("api.urls")),
+    path("api-auth/login/", include("api.urls")),
     path("", include("accounts.urls")),
     # path("api/", include('router.urls')),
     # path("api-token-auth/", views.obtain_auth_token),
