eee5fe403b4204098c6fb631fefd0c3e0c8ee557
Taka <takahito.uchino.1218@gmail.com>
2024-03-02 14:12:02 +0900

    画像データの送信

--- backend/django_app/django_app/settings.py
+++ backend/django_app/django_app/settings.py
@@ -178,7 +178,7 @@ AWS_STORAGE_BUCKET_NAME = ""
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-AWS_DEFAULT_ACL = "public-read"
+AWS_DEFAULT_ACL = None
 
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
